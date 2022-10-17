3aa9ea718dfd0580507bb232f17b263078e5de0f
Emil Larsson <emil.larsson@navigaglobal.com>
2022-10-17 11:22:31 +0200

    Add mac configuration with yabai

---
+++ mac/skhd/openNewTerminal.sh
@@ -0,0 +1,6 @@
+
+osascript -e "
+      tell application \"iTerm2\"
+        set newWindow to (create window with default profile)
+      end tell
+    "
---
+++ mac/skhd/startChrome.sh
@@ -0,0 +1,7 @@
+
+osascript -e "
+	tell application \"/Applications/Google Chrome.app\"
+	    make new window
+	    activate
+	end tell
+"
